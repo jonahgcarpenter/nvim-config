75b55213c7de024427ac72697c11759bc6296da5
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-02-05 12:20:23 -0600

    indentation changes

--- lua/jonah/core/settings.lua
+++ lua/jonah/core/settings.lua
@@ -6,10 +6,10 @@ vim.opt.relativenumber = true
 
 -- indenting
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 -- line wrap sucks
 vim.opt.wrap = false
