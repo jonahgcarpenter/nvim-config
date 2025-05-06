ad556507c980db1f1f858a35e616b1e8891ccfdd
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-05-05 22:07:20 -0500

    no more copilot

--- lua/jonah/plugins/init.lua
+++ lua/jonah/plugins/init.lua
@@ -1,6 +1,5 @@
 return {
-    "tpope/vim-fugitive", -- fugitive for git
-    "github/copilot.vim", -- who doesnt use copilot in 2025
-    "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-    "christoomey/vim-tmux-navigator", -- tmux/split window navigation
-  }
+	"tpope/vim-fugitive", -- fugitive for git
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux/split window navigation
+}
